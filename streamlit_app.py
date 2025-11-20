5f4465724426353ed4527a0b45d4106845dfe2e1
jpaglia72 <joe_pagz@yahoo.com>
2025-11-19 23:05:28 -0500

    added pandas sample

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
